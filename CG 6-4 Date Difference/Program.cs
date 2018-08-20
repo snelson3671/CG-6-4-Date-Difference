5fe4fc9b1bdd0fc0bd10405e7e49156763c3870a
Sandra <snelson3671@yahoo.com>
2018-08-19 21:04:07 -0500

    Add project files.

---
+++ CG 6-4 Date Difference/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_6_4_Date_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
